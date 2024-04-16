706a9e7103eaac06ef921639fbf30e1b0eed717a
YazminReyes <ryazru1993@gmail.com>
2024-04-16 08:21:14 -0600

    Se agrego la grafica de corriente

---
+++ energy_watch/src/app/views/charts/echarts/EchartCorriente.jsx
@@ -0,0 +1,50 @@
+import { Box, styled, useTheme } from "@mui/material";
+
+import Breadcrumb from "app/components/Breadcrumb";
+import SimpleCard from "app/components/SimpleCard";
+
+import AreaChart from "./AreaChart";
+import LineChart from "./LineChartC";
+import DoughnutChart from "./Doughnut";
+import ComparisonChart from "./ComparisonChart";
+
+// STYLED COMPONENT
+const Container = styled("div")(({ theme }) => ({
+  margin: 30,
+  [theme.breakpoints.down("sm")]: { margin: 16 },
+  "& .breadcrumb": {
+    marginBottom: 30,
+    [theme.breakpoints.down("sm")]: { marginBottom: 16 },
+  },
+}));
+
+export default function AppEchart() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <h1>CORRIENTE</h1>
+      <Box className="breadcrumb">
+        <Breadcrumb
+          routeSegments={[
+            { name: "Charts", path: "/charts" },
+            { name: "Echarts" },
+          ]}
+        />
+      </Box>
+
+      <Box sx={{ py: "12px" }} />
+
+      <SimpleCard title="Line Chart">
+        <LineChart
+          height="350px"
+          color={[theme.palette.primary.main, theme.palette.primary.light]}
+        />
+      </SimpleCard>
+
+      <Box sx={{ py: "12px" }} />
+
+      <Box sx={{ py: "12px" }} />
+    </Container>
+  );
+}
